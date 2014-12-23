59886fe5bb8188d006e2d095acc01b1dda33d864
Christian Guedemann <christian.guedemann@webgate.biz>
2014-12-23 18:16:14 +0100

    NO-SYSTEM Version tested and as good approved

--- org.openntf.base.jaxb.dxl.testsuite/src/org/openntf/base/jaxb/dxl/testsuite/ConverterFactoryTest.java
+++ org.openntf.base.jaxb.dxl.testsuite/src/org/openntf/base/jaxb/dxl/testsuite/ConverterFactoryTest.java
@@ -4,15 +4,37 @@ import java.io.InputStream;
 
 import org.junit.Test;
 import org.openntf.base.jaxb.dxl.Document;
+import org.openntf.base.jaxb.dxl.Item;
+import org.openntf.base.jaxb.dxl.Richtext;
 import org.openntf.base.jaxb.utils.ConvertorFactory;
+
 import static org.junit.Assert.*;
 
 public class ConverterFactoryTest {
 
 	@Test
 	public void testConvertDXLTestDocumentToDocument() {
-		InputStream isXML = ConverterFactoryTest.class.getResourceAsStream("dxl-document-test.xml");
-		Document docConverted = ConvertorFactory.convert2DocumentFromStream(isXML);
+		InputStream isXML = ConverterFactoryTest.class.getResourceAsStream("dxl-document-test-nosys.xml");
+		Document docConverted = ConvertorFactory.convert2DXLDocumentFromStream(isXML);
 		assertNotNull(docConverted);
+	}
+	
+	@Test
+	public void testFindRTItemInTestDocument() {
+		InputStream isXML = ConverterFactoryTest.class.getResourceAsStream("dxl-document-test-nosys.xml");
+		Document docConverted = ConvertorFactory.convert2DXLDocumentFromStream(isXML);
+		assertNotNull(docConverted);
+		Richtext richText = null;
+		System.out.println(docConverted.getItem().size());
+		System.out.println(docConverted.getNoteinfo());
+		System.out.println(docConverted.getUpdatedby());
+		for (Item itemcheck :docConverted.getItem()) {
+			System.out.println(itemcheck.getName());
+			if (itemcheck.getRichtext() != null){
+				richText = itemcheck.getRichtext();
+				break;
+			}
+		}
+		assertNotNull(richText);
 	}
 }
--- org.openntf.base.jaxb.dxl/src/org/openntf/base/jaxb/utils/ConvertorFactory.java
+++ org.openntf.base.jaxb.dxl/src/org/openntf/base/jaxb/utils/ConvertorFactory.java
@@ -1,14 +1,37 @@
 package org.openntf.base.jaxb.utils;
 
 import java.io.InputStream;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 import org.openntf.base.jaxb.dxl.Document;
 
 public class ConvertorFactory {
 
-	public static Document convert2DocumentFromStream(InputStream isXML) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Document convert2DXLDocumentFromStream(final InputStream isXML) {
+		Document dxlDocument = AccessController.doPrivileged(new PrivilegedAction<Document>() {
+
+			@Override
+			public Document run() {
+				JAXBContext context;
+				try {
+					context = JAXBContext.newInstance(Document.class);
+					Unmarshaller unmarshaler = context.createUnmarshaller();
+					StreamSource source = new StreamSource(isXML);
+					JAXBElement<Document> root = unmarshaler.unmarshal(source, Document.class);
+					return root.getValue();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		});
+		return dxlDocument;
 	}
 
 }
